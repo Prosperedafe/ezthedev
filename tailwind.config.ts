1e94aeeb9101e162e68fff8ac4e8ea1370114750
prosper edafe <mamusedafe19@gmail.com>
2024-08-05 13:31:16 +0100

    project setup completed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,32 +7,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      screens: {
-        'xxl': '1350px',
-        'xl': '1280px',
-        'lg': '1024px',
-        'md': '768px',
-        'sm': '640px',
-        'ssm': '420px',
-        'lsm': '320px'
-      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        "primary": "hsla(208, 50%, 41%, 1)",
-        "light-100": "#fff",
+        "primaryBg": "#F4F3EE",
+        "primaryText": "#3D403C",
         "light-200": "hsla(210, 17%, 98%, 1)",
         "light-300": "hsla(208, 50%, 98%, 1)",
         "dark-200": "hsla(0, 0%, 31%, 1)",
         "text-disabled": "hsla(228, 3%, 66%, 1)",
       },
       fontFamily: {
-        montserrat: ['Inter', 'sans-serif'],
-        proxima: ['Proxima', 'sans-serif'],
-        Sans: ['Source Sans', 'sans-serif']
+        Trial: ['Trial', 'sans-serif']
       },
     },
   },
